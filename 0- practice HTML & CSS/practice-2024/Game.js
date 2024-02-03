6a5168797eafc0adde508bed6511eb94a5b8f1df
YBO <yitzhak851@gmail.com>
2024-02-03 16:11:05 +0200

    Practice java script

---
+++ 0- practice HTML & CSS/practice-2024/Game.js
@@ -0,0 +1,34 @@
+class Game {
+  constructor(firstPlayer) {
+    this.boardData = new BoardData();
+    this.currentPlayer = firstPlayer;
+    this.winner = undefined;
+  }
+
+  // Tries to actually make a move. Returns true if successful.
+  tryMove(piece, row, col) {
+    const possibleMoves = this.getPossibleMoves(piece);
+
+    for (const possibleMove of possibleMoves) {
+      if (possibleMove[0] === row && possibleMove[1] === col) {
+        const removedPiece = this.boardData.removePiece(row, col);
+        piece.row = row;
+        piece.col = col;
+        if (removedPiece !== undefined && removedPiece === KING) {
+          this.winner = piece.player;
+        }
+
+        this.currentPlayer = piece.getOpponent();
+        return true;
+      }
+    }
+    return false;
+  }
+
+  getPossibleMoves(piece) {
+    if (this.currentPlayer !== piece.player || this.winner !== undefined) {
+      return [];
+    }
+    return piece.getPossibleMoves(this.boardData);
+  }
+}
---
+++ 0- practice HTML & CSS/practice-2024/app.js
@@ -0,0 +1,59 @@
+//declrate variables
+var board = document.getElementById('boardInner');
+var numContainer = document.getElementById('numContainer');
+var letterContainer = document.getElementById('lettContainer');
+var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8']
+var display = 'block'
+
+const renderBoard = (useLabels) => {
+    if (useLabels) {
+        renderLabels()
+    }
+    var change = false
+    var color = 'white'
+    for (var i = 0; i < 64; ++i) {
+        var el = document.createElement('DIV')
+        el.className = 'square'
+        change = i % 8 === 0 || i % 8 === NaN
+        color = change ? color :
+            color === 'white' ? 'black' : 'white'
+        el.style.backgroundColor = color
+        board.appendChild(el)
+        change = false
+    }
+}
+
+const renderBoard1 = (useLabels) => {
+    if (useLabels) {
+        renderLabels()
+    }
+    for (var i = 0; i < letters.length; ++i) {
+        var row = document.createElement('DIV')
+        row.className = 'row'
+        row.style.flexDirection = i % 2 === 0 ? '' : 'row-reverse';
+        for (var j = 0; j < letters.length; ++j) {
+            var square = document.createElement('DIV')
+            square.className = 'square'
+            square.style.backgroundColor = j % 2 === 0 ? 'white' : 'black'
+            row.appendChild(square)
+        }
+        board.appendChild(row)
+    }
+}
+
+renderLabels = () => {
+    letters.forEach((x, i) => {
+        var el = document.createElement('DIV')
+        var el2 = document.createElement('DIV')
+        el.innerText = x
+        el.className = 'label'
+        el2.innerText = numbers[i]
+        el2.className = 'label'
+        numContainer.appendChild(el2)
+        letterContainer.appendChild(el)
+    })
+}
+
+
+renderBoard1()
