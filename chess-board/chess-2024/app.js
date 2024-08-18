6bf8f8f9fdf066f5b1c25697d07102d55c5cb084
Yitzhak851 <yitzhak851@gmail.com>
2024-08-18 12:49:40 +0300

    adding board

---
+++ chess-board/chess-2024/app.js
@@ -0,0 +1,30 @@
+// create the fields of the boards
+const BOARD_SIZE = 8;
+
+// the fofo create chess board
+function createChessBoard(){
+    // create all the board by table (row's and colom's)
+    const table = document.createElement('table');
+    document.body.appendChild(table1);
+    for (let i = 0; i < BOARD_SIZE; i++) {
+        const row = document.createElement('tr');
+        table1.body.appendChild(row);
+        // const row = table1.insertRow(i);
+        for (let j = 0; j < BOARD_SIZE; j++) {
+            const cell = document.createElement('tc');
+            table1.body.appendChild(cell);
+            // const cell = table1.insertCell(j);
+            cell.textContent = '123';
+            if(i+j%2 == 0){
+                cell.className = 'light-cell';
+            }
+            cell.className = 'dark-cell'
+        }
+    }
+}
+
+// call 
+window.addEventListener('load', createChessBoard() );
+
+
+
